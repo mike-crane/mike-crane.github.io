19f6298bd775bbdf479bf852fb28316649eeb984
Mike Crane <mike.c0321@gmail.com>
2016-05-19 22:31:29 -0400

    Edit About Me & clean up script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,42 +1,51 @@
-$(document).ready(function(){
+$(document).ready(function () {
 
-    $(".jumper").click(function() {
-        $('html, body').animate({
-            scrollTop: $("href").offset().top
-        }, 2000);
+
+    $('a[href*="#"]:not([href="#"])').click(function () {
+        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+            if (target.length) {
+                $('html, body').animate({
+                    scrollTop: target.offset().top
+                }, 1500);
+                return false;
+            }
+        }
     });
 
 
-	$('.project1').mouseenter(function(){
-		$(this).stop().fadeTo('slow',0.4);
-	}).mouseleave(function(){
-		$(this).stop().fadeTo('slow',1);
-	});
 
-    $('.project2').mouseenter(function(){
-		$(this).stop().fadeTo('slow',0.4);
-	}).mouseleave(function(){
-		$(this).stop().fadeTo('slow',1);
-	});
-
-    $('.project3').mouseenter(function(){
-		$(this).stop().fadeTo('slow',0.4);
-	}).mouseleave(function(){
-		$(this).stop().fadeTo('slow',1);
-	});
-
-
-
-    $(".project1").click(function(){
-             window.location = "https://github.com/foodstream";
+    $('.project1').mouseenter(function () {
+        $(this).stop().fadeTo('slow', 0.4);
+    }).mouseleave(function () {
+        $(this).stop().fadeTo('slow', 1);
     });
 
-    $(".project2").click(function(){
-             window.location = "https://github.com/mike-crane/memory-layout";
+    $('.project2').mouseenter(function () {
+        $(this).stop().fadeTo('slow', 0.4);
+    }).mouseleave(function () {
+        $(this).stop().fadeTo('slow', 1);
     });
 
-    $(".project3").click(function(){
-             window.location = "https://github.com/mike-crane/responsive-icon-grid";
+    $('.project3').mouseenter(function () {
+        $(this).stop().fadeTo('slow', 0.4);
+    }).mouseleave(function () {
+        $(this).stop().fadeTo('slow', 1);
+    });
+
+
+
+    $(".project1").click(function () {
+        window.location = "https://github.com/foodstream";
+    });
+
+    $(".project2").click(function () {
+        window.location = "https://github.com/mike-crane/memory-layout";
+    });
+
+    $(".project3").click(function () {
+        window.location = "https://github.com/mike-crane/responsive-icon-grid";
     });
 
 });
